ab49d55e658371604212c27ab99dc49caa6d312b
1927930563@qq.com <1927930563@qq.com>
2026-05-05 17:24:51 +0800

    add @netlify/plugin-nextjs, remove netlify.toml for auto-detect

---
+++ .netlify/static/_next/static/chunks/app/layout-886b9dc8c6d4fdf6.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{1290:()=>{},5536:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,1290,23)),Promise.resolve().then(n.bind(n,8720))}},e=>{e.O(0,[741,720,441,255,358],()=>e(e.s=5536)),_N_E=e.O()}]);
